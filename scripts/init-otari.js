075f0c53f3b305d40ce84f1df9951eb36013f307
mark pearce <mark.pearce72@gmail.com>
2022-05-30 09:45:13 +0200

    Added all paths - scene crasher
    works nicely until you change scenes,
    the the update loop crashes the canvas renderer!

--- scripts/init-otari.js
+++ scripts/init-otari.js
@@ -16,13 +16,21 @@ let ghostContainer = new PIXI.Container()
 let ghostNull = new PIXI.Container()
 let ghost //sprite
 let ghostLight
-let ghostEmitter
-let footprintsEmitter
-let pathOne =
-  'M1603,2474c36,7.5,90.2,8.7,111.9-21.2c24-33-20.3-69,3.5-177.5c18.3-83.6,95-103.1,154.7-99.4 c110,7,141.5-8.5,204.5,126.5c49.5,106.1,265.9-58.5,304.5-219c20.5-85.5-396-105-327,19.5c50.5,91.1,132.8,44.6,184.5,66 c32.6,13.5,21.5,146,49.5,154.5c57.9,17.6,95.3-25.5,124.5-97.5c32.9-81-3.2-227.3,53.5-202c65,29-7.5,152.1,43.9,159.4 c80.4,11.5,167,10.3,214.1-14.4c63.1-33.2,24.9-131.7,161.4-140.7s180,22.5,484.5-33c151.9-27.7,273-15,435,0 s160.7-51.1,169.5-103.5c7.9-46.9,16.8-125-45.4-203.8'
+let ghostEmitter // aura particles
+let leftPrintEmitter
+let rightPrintEmitter
+let stepFrequency = 1.5
 
-let pathTwo =
+let ghostPath
+
+let levelTwo =
+  'M5266.7,3306.7c62.2,78.9,66.6,188.3,58.7,235.2c-8.8,52.4-7.5,118.5-169.5,103.5s-283.1-27.7-435,0 c-304.5,55.5-348,24-484.5,33s-81,134.3-152.1,140c-50.6,4-159.7,38.2-229.6,1.7c-35.9-18.8,21-139.8-64-146.7 c-36.6-2.9,5.7,121.7-27.2,202.7c-29.2,72-66.6,115.1-124.5,97.5c-28-8.5-16.9-141-49.5-154.5c-51.7-21.4-134,25.1-184.5-66 c-69-124.5,347.5-105,327-19.5c-38.6,160.5-255,325.1-304.5,219c-63-135-184.4-127.6-201.9-129c-108-9-138.9,18.3-157.2,101.9 c-23.8,108.5,12.2,153-11.8,186c-21.7,29.8-97.9,35.3-123.2-6c-19.7-32.2-21.3-214.7-12-265.3'
+
+let levelThree =
   'M2370,6243c-108,0-168,18-189-66s-18-291,0-345s120,7.5,162-141c21.7-76.9,57-63,159-63s327,13.5,373.5-12 s57-97.5,57-157.5s8.1-113.8-4.5-96c-18,25.5-19.5,102-10.5,145.5s-7.5,105,61.5,120s235.5,4.5,366,7.5s244.3,20.5,305.7-76.8 c59.1-93.8,114.7-192,133.3-512c11.7-201-91.3-299.8,34.7-385.3S4254,4551,4446,4575s58.9,223.2,242,289 c59.3,21.3,189.3,25.3,362.7,4c104.5-12.9,260,4.9,393.3,10.7c189.3,8.2,349.1,8.5,386.7,2.7c149.9-23.4,70.1-240.5,141.3-294.7 c25.3-19.3,213.3-19.3,238,24.3c35,61.9,14.4,329.2,6,372'
+
+let levelFour =
+  'M6034.5,5625c-127.5,4.5-249-18-363-1.5s-258,39-321-15s-33-292.5-66-358.5s-55.5,40.5-112.5,3 s-171,25.5-145.5,81s154.5,72,187.5,121.5s-126.4,19.7-156,49.5c-31.3,31.5,97.5,112.5,46.5,123s-228.9-89.1-247.5-106.5 c-36.5-34.1-127.3,26.4-139.5,100.5c-12.1,73.7-145.5,72-157.5,118.5s-67.5,378-42,528s-55.5,325.5-81,439.5 c-20.4,91.1,39,274.5,27,429s43.5,304.5,82.5,342s207,126,307.5,105s228-43.5,370.5,19.5S5525,7584,5525,7584'
 
 /////////////////////////////
 //    API DEFININITION     //
@@ -31,21 +39,10 @@ let pathTwo =
 class FootstepsOfOtari {
   //API Functions below : must be registered with API and SOCKET
 
-  static doTheThing(argument, userID = null) {
+  static doTheThing(argument) {
     // does stuff you want other modules to have access to
-    console.log('the thing is: ' + argument)
-    if (argument == 'doStuff') {
-      if (userID != null) {
-        socket.executeForUsers(doStuff, [userID])
-      } else {
-        socket.executeForEveryone(doStuff)
-      }
-    }
-  }
-
-  static makeCircle() {
-    console.log('make a circle')
-    socket.executeForEveryone(createPixi)
+    console.log('play floor: ' + argument)
+    socket.executeForEveryone(doStuff, [argument])
   }
 
   static makeGhost() {
@@ -66,7 +63,6 @@ Hooks.on('init', function () {
   // once set up, we create our API object. Each function needs an entry
   game.modules.get('footsteps-of-otari').api = {
     doTheThing: FootstepsOfOtari.doTheThing,
-    makeCircle: FootstepsOfOtari.makeCircle,
     makeGhost: FootstepsOfOtari.makeGhost,
     nameChild: FootstepsOfOtari.nameChild,
   }
@@ -86,7 +82,6 @@ Hooks.once('socketlib.ready', () => {
   socket = socketlib.registerModule('footsteps-of-otari')
   //register socket functions
   socket.register('doStuff', doStuff)
-  socket.register('createPixi', createPixi)
   socket.register('createGhost', createGhost)
   socket.register('nameChild', nameChild)
 })
@@ -95,28 +90,43 @@ Hooks.once('socketlib.ready', () => {
 //    API FUNCTIONS     //
 //////////////////////////
 
-function doStuff() {
+function doStuff(whatLevel) {
+  console.log('do level:' + whatLevel)
+  //let ghostPath
+  let pathDuration
+
+  if (whatLevel == 'two') {
+    console.log('its fucking 2')
+    ghostPath = levelTwo
+    pathDuration = 36
+  }
+
+  if (whatLevel == 'three') {
+    console.log('its fucking 3')
+    ghostPath = levelThree
+    pathDuration = 66
+  }
+
+  if (whatLevel == 'four') {
+    console.log('its fucking 4')
+    ghostPath = levelFour
+    pathDuration = 48
+  }
+
+  console.log('ghostPath: ' + ghostPath)
+  console.log('pathDuration: ' + pathDuration)
   gsap.to(ghostNull, {
-    motionPath: { path: pathTwo, autoRotate: 90 },
-    duration: 66,
+    motionPath: { path: ghostPath, autoRotate: 90 },
+    duration: pathDuration,
     ease: 'none',
   })
-}
-
-function createPixi() {
-  console.log('createPixi')
-  const g = new PIXI.Graphics()
-  g.beginFill(0x55e08f)
-  g.drawCircle(100, 200, 100)
-  g.endFill()
-  canvas.app.stage.addChild(g)
 }
 
 async function createGhost() {
   console.log('createGhost')
   //ghost sprite
   const ghostTexture = await loadTexture(
-    'modules/footsteps-of-otari/artwork/ghost-blob-arrow.webp',
+    'modules/footsteps-of-otari/artwork/ghost-blob.webp',
   )
   ghost = new PIXI.Sprite(ghostTexture)
   ghost.name = 'otariOne'
@@ -128,8 +138,9 @@ async function createGhost() {
   ghost.height = 48
   ghostNull.addChild(ghost)
 
-  // Ghost Aura Particles
   let now = Date.now()
+
+  // Ghost Aura Particles
   ghostEmitter = new PIXI.particles.Emitter(
     // The PIXI.Container to put the emitter in
     ghostContainer,
@@ -140,9 +151,9 @@ async function createGhost() {
       alpha: { start: 1, end: 0 },
       scale: { start: 0.5, end: 0.1, minimumScaleMultiplier: 1 },
       color: { start: '#4af095', end: '#169e0c' },
-      speed: { start: 20, end: 5, minimumSpeedMultiplier: 0 },
+      speed: { start: 20, end: 5, minimumSpeedMultiplier: 1 },
       acceleration: { x: 0, y: 0 },
-      maxSpeed: 0,
+      maxSpeed: 20,
       startRotation: { min: 0, max: 0 },
       noRotation: false,
       rotationSpeed: { min: 0, max: 0 },
@@ -159,51 +170,87 @@ async function createGhost() {
 
   ghostEmitter.emit = true
 
-  // Footstep Particles
+  // Left Footstep Particles
   const leftFoot = await loadTexture(
     'modules/footsteps-of-otari/artwork/ghost-left.webp',
   )
-  const rightFoot = await loadTexture(
-    'modules/footsteps-of-otari/artwork/ghost-left.webp',
-  )
 
-  footprintsEmitter = new PIXI.particles.Emitter(
+  leftPrintEmitter = new PIXI.particles.Emitter(
     // The PIXI.Container to put the emitter in
     ghostContainer,
     // The collection of particle images
-    [leftFoot, rightFoot],
+    [leftFoot],
     // Emitter configuration
     {
       alpha: { start: 1, end: 0 },
       scale: { start: 0.5, end: 0.1, minimumScaleMultiplier: 1 },
       color: { start: '#4af095', end: '#169e0c' },
-      speed: { start: 0, end: 0, minimumSpeedMultiplier: 0 },
+      speed: { start: 0.01, end: 0, minimumSpeedMultiplier: 0 },
       acceleration: { x: 0, y: 0 },
-      maxSpeed: 0,
-      startRotation: { min: 0, max: 360 },
+      maxSpeed: 0.01,
+      startRotation: { min: 0, max: 0 },
       noRotation: false,
       rotationSpeed: { min: 0, max: 0 },
       lifetime: { min: 16, max: 18 },
       blendMode: 'add',
-      frequency: 0.5,
+      frequency: stepFrequency,
       emitterLifetime: -1,
-      maxParticles: 500,
-      pos: { x: 0, y: 0 },
+      maxParticles: 100,
+      pos: { x: 6, y: 0 },
       addAtBack: true,
       spawnType: 'point',
     },
   )
 
-  footprintsEmitter.emit = true
+  leftPrintEmitter.emit = true
 
+  // Right Footstep Particles
+  const rightFoot = await loadTexture(
+    'modules/footsteps-of-otari/artwork/ghost-right.webp',
+  )
+
+  rightPrintEmitter = new PIXI.particles.Emitter(
+    // The PIXI.Container to put the emitter in
+    ghostContainer,
+    // The collection of particle images to use
+    [rightFoot],
+    // Emitter configuration
+    {
+      alpha: { start: 1, end: 0 },
+      scale: { start: 0.5, end: 0.1, minimumScaleMultiplier: 1 },
+      color: { start: '#4af095', end: '#169e0c' },
+      speed: { start: 0.01, end: 0, minimumSpeedMultiplier: 0 },
+      acceleration: { x: 0, y: 0 },
+      maxSpeed: 0.1,
+      startRotation: { min: 0, max: 0 },
+      noRotation: false,
+      rotationSpeed: { min: 0, max: 0 },
+      lifetime: { min: 20, max: 22 },
+      blendMode: 'add',
+      frequency: stepFrequency,
+      emitterLifetime: -1,
+      maxParticles: 100,
+      pos: { x: 26, y: 0 },
+      addAtBack: true,
+      spawnType: 'point',
+    },
+  )
+
+  rightPrintEmitter.emit = false
+  gsap.delayedCall(stepFrequency / 2, startRight)
   // Particle updater
   canvas.app.ticker.add(() => {
     const newNow = Date.now()
     ghostNull.rotation = ghostNull.rotation * (Math.PI / 180)
     ghostEmitter.update((newNow - now) * 0.001)
     ghostEmitter.updateOwnerPos(ghostNull.x, ghostNull.y)
-    footprintsEmitter.update((newNow - now) * 0.001)
-    footprintsEmitter.updateOwnerPos(ghostNull.x, ghostNull.y)
+    ghostEmitter.rotate(ghostNull.rotation * (180 / Math.PI))
+    leftPrintEmitter.update((newNow - now) * 0.001)
+    leftPrintEmitter.updateOwnerPos(ghostNull.x, ghostNull.y)
+    leftPrintEmitter.rotate(ghostNull.rotation * (180 / Math.PI))
+    rightPrintEmitter.update((newNow - now) * 0.001)
+    rightPrintEmitter.updateOwnerPos(ghostNull.x, ghostNull.y)
+    rightPrintEmitter.rotate(ghostNull.rotation * (180 / Math.PI))
     now = newNow
   })
 
@@ -214,6 +261,19 @@ async function createGhost() {
   canvas.background.addChild(ghostContainer)
 }
 
+///////////////////////////////
+//    INTERNAL FUNCTIONS     //
+///////////////////////////////
+
+function startRight() {
+  rightPrintEmitter.emit = true
+  gsap.killTweensOf(startRight)
+}
+
+///////////////////////////////////
+//    DEBUG & TEMP FUNCTIONS     //
+///////////////////////////////////
+
 async function nameChild() {
   if (game.user.isGM) {
     console.log('nameChild' + childNum)
@@ -221,7 +281,3 @@ async function nameChild() {
     childNum++
   }
 }
-
-///////////////////////////////
-//    INTERNAL FUNCTIONS     //
-///////////////////////////////
